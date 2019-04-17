d831a25141e0a01568e82a831867415bb330b0b0
Jess Frazelle <acidburn@jessfraz.com>
2019-04-17 02:29:17 -0400

    slower
    Signed-off-by: Jess Frazelle <acidburn@jessfraz.com>

--- static/js/circuit.js
+++ static/js/circuit.js
@@ -13,7 +13,7 @@ var paths = document.querySelectorAll('.st0');
     path.getBoundingClientRect();
     // Define our transition
     path.style.transition = path.style.WebkitTransition =
-        'stroke-dashoffset 2s ease-in-out';
+        'stroke-dashoffset 4s ease-in-out';
     // Go!
     path.style.strokeDashoffset = '0';
 })
