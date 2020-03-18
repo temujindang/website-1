697a6e9cd15b49a42ba8e23498a5fe538572d461
Jess Frazelle <jess@oxide.computer>
2020-03-18 13:22:39 -0700

    try fix fonts
    Signed-off-by: Jess Frazelle <jess@oxide.computer>

--- src/utils/minify-css.js
+++ src/utils/minify-css.js
@@ -5,4 +5,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("cssmin", function (code) {
     return new CleanCSS({}).minify(code).styles;
   });
+
+  // Copy `css/fonts/` to `_site/css/fonts`
+  // If you use a subdirectory, it’ll copy using the same directory structure.
+  eleventyConfig.addPassthroughCopy("css/fonts");
 };
