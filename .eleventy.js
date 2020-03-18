cc3e768270f2608247bf0dc7c4ef4a7ebf7b2007
Jess Frazelle <jess@oxide.computer>
2020-03-18 13:39:31 -0700

    update
    Signed-off-by: Jess Frazelle <jess@oxide.computer>

--- .eleventy.js
+++ .eleventy.js
@@ -59,7 +59,7 @@ module.exports = function (eleventyConfig) {
   };
 
   // Static assets to pass through
-  eleventyConfig.addPassthroughCopy("./src/site/css/fonts");
+  eleventyConfig.addPassthroughCopy("./src/site/fonts");
   eleventyConfig.addPassthroughCopy("./src/site/images");
   eleventyConfig.addPassthroughCopy("./src/site/css");
 
