974979b7d2dbf5e2dae4ea1255b1995ccd373182
Jess Frazelle <jess@oxide.computer>
2020-03-20 10:13:06 -0700

    scroll to
    Signed-off-by: Jess Frazelle <jess@oxide.computer>

--- src/site/_includes/js/elevator.js
+++ src/site/_includes/js/elevator.js
@@ -1,27 +1,30 @@
-function scrollTo(element, to, duration) {
-    var start = element.scrollTop,
+var
+scrollTo = function(to, duration) {
+    const
+    element = document.scrollingElement || document.documentElement,
+        start = element.scrollTop,
         change = to - start,
-        currentTime = 0,
-        increment = 20;
-
-    var animateScroll = function(){
-        currentTime += increment;
-        var val = Math.easeInOutQuad(currentTime, start, change, duration);
-        element.scrollTop = val;
-        if(currentTime < duration) {
-            setTimeout(animateScroll, increment);
-        }
-    };
+        startDate = +new Date(),
+        // t = current time
+        // b = start value
+        // c = change in value
+        // d = duration
+        easeInOutQuad = function(t, b, c, d) {
+            t /= d/2;
+            if (t < 1) return c/2*t*t + b;
+            t--;
+            return -c/2 * (t*(t-2) - 1) + b;
+        },
+        animateScroll = function() {
+            const currentDate = +new Date();
+            const currentTime = currentDate - startDate;
+            element.scrollTop = parseInt(easeInOutQuad(currentTime, start, change, duration));
+            if(currentTime < duration) {
+                requestAnimationFrame(animateScroll);
+            }
+            else {
+                element.scrollTop = to;
+            }
+        };
     animateScroll();
-}
-
-//t = current time
-//b = start value
-//c = change in value
-//d = duration
-Math.easeInOutQuad = function (t, b, c, d) {
-    t /= d/2;
-    if (t < 1) return c/2*t*t + b;
-    t--;
-    return -c/2 * (t*(t-2) - 1) + b;
-};/* this is not elevator js it is a custom script */
+};
