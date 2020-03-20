64fb395bc27dd0f528e1e80484526c0e88372ae1
Jess Frazelle <jess@oxide.computer>
2020-03-20 10:06:38 -0700

    update podcast page
    Signed-off-by: Jess Frazelle <jess@oxide.computer>

--- src/site/_includes/js/hello.js
+++ src/site/_includes/js/hello.js
@@ -2,48 +2,38 @@
 //   console.log('👋 Oh, hello there you!');
 // });
 
-// var elevator = new Elevator({
-//   element: document.querySelector('.elevator-button'),
-//   duration: 1000 // milliseconds
-// });
-
-// // Simple elevator usage.
-// var elementButton = document.querySelector('.elevator');
-// var elevator = new Elevator({
-//   element: elementButton,
-//   mainAudio: './music/elevator.mp3', // Music from http://www.bensound.com/
-//   endAudio: './music/ding.mp3'
-// });
+// Code for back to top button.
+document.querySelector('.elevator-button').addEventListener('click', () => scrollTo(document.body, 0, 1250));
 
 var paths = document.querySelectorAll('.st0');
 [].forEach.call(paths, function (path) {
-  var length = path.getTotalLength();
-  path.style.transition = path.style.WebkitTransition = 'none';
-  path.style.strokeDasharray = length + ' ' + length;
-  path.style.strokeDashoffset = length;
-  path.getBoundingClientRect();
-  path.style.transition = path.style.WebkitTransition = 'stroke-dashoffset 4s ease-in-out';
-  path.style.strokeDashoffset = '0';
+    var length = path.getTotalLength();
+    path.style.transition = path.style.WebkitTransition = 'none';
+    path.style.strokeDasharray = length + ' ' + length;
+    path.style.strokeDashoffset = length;
+    path.getBoundingClientRect();
+    path.style.transition = path.style.WebkitTransition = 'stroke-dashoffset 4s ease-in-out';
+    path.style.strokeDashoffset = '0';
 });
 
 // Detect request animation frame
 var scroll = window.requestAnimationFrame ||
-  // IE Fallback
-  function (callback) { window.setTimeout(callback, 1000 / 60) };
+    // IE Fallback
+    function (callback) { window.setTimeout(callback, 1000 / 60) };
 
 var elementsToShow = document.querySelectorAll('.show-on-scroll');
 
 function loop() {
 
-  Array.prototype.forEach.call(elementsToShow, function (element) {
-    if (isElementInViewport(element)) {
-      element.classList.add('is-visible');
-    } else {
-      element.classList.remove('is-visible');
-    }
-  });
+    Array.prototype.forEach.call(elementsToShow, function (element) {
+        if (isElementInViewport(element)) {
+            element.classList.add('is-visible');
+        } else {
+            element.classList.remove('is-visible');
+        }
+    });
 
-  scroll(loop);
+    scroll(loop);
 }
 
 // Call the loop for the first time
@@ -52,17 +42,17 @@ loop();
 // Helper function from: http://stackoverflow.com/a/7557433/274826
 function isElementInViewport(el) {
 
-  var rect = el.getBoundingClientRect();
-  return (
-    (rect.top <= 0
-      && rect.bottom >= 0)
-    ||
-    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
-    ||
-    (rect.top >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
-  );
+    var rect = el.getBoundingClientRect();
+    return (
+        (rect.top <= 0
+            && rect.bottom >= 0)
+        ||
+        (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+        ||
+        (rect.top >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+    );
 }
 
 // var hero = document.getElementById('hero').clientHeight;
